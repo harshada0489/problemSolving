a0c92972a42b1d09442aafd5cb453984c6e66838
harshada <harshada2@unl.edu>
2022-05-19 14:41:40 -0700

    2nd day - Array

--- 217_ContainsDuplicate.java
+++ 217_ContainsDuplicate.java
@@ -1,4 +1,8 @@
+// https://leetcode.com/problems/contains-duplicate/
+
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+//Example
 // Input: nums = [1,2,3,1]
 // Output: true
 
--- 53_Maximum_Subarray.java
+++ 53_Maximum_Subarray.java
@@ -1,11 +1,12 @@
-// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// https://leetcode.com/problems/maximum-subarray/
 
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 // A subarray is a contiguous part of an array.
 
+//Example
 // Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 // Output: 6
 // Explanation: [4,-1,2,1] has the largest sum = 6.
-
 
 class Solution {
     public int maxSubArray(int[] nums) { 
